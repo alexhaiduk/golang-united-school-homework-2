54f0926ce655afe578866787e50ae53f70e070c2
alexhaiduk <alexhaiduk82@gmail.com>
2022-02-19 00:42:55 +0300

    solution switch

--- solution.go
+++ solution.go
@@ -20,26 +20,26 @@ const (
 
 //func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
 func CalcSquare(sideLen float64, sidesNum Sides) float64 {
-	//var result float64
-	if sidesNum == SidesCircle {
-		return math.Pi * math.Pow(sideLen, 2)
-	} else if sidesNum == SidesTriangle {
-		return (math.Sqrt(3) * math.Pow(sideLen, 2)) / 4
-	} else if sidesNum == SidesSquare {
-		return math.Pow(sideLen, 2)
-	} else {
-		return 0
-	}
 	/*
-		switch sidesNum {
-		default:
-			return 0
-		case SidesCircle:
+		//var result float64
+		if sidesNum == SidesCircle {
 			return math.Pi * math.Pow(sideLen, 2)
-		case SidesTriangle:
+		} else if sidesNum == SidesTriangle {
 			return (math.Sqrt(3) * math.Pow(sideLen, 2)) / 4
-		case SidesSquare:
+		} else if sidesNum == SidesSquare {
 			return math.Pow(sideLen, 2)
+		} else {
+			return 0
 		}
 	*/
+	switch sidesNum {
+	default:
+		return 0
+	case SidesCircle:
+		return math.Pi * math.Pow(sideLen, 2)
+	case SidesTriangle:
+		return (math.Sqrt(3) * math.Pow(sideLen, 2)) / 4
+	case SidesSquare:
+		return math.Pow(sideLen, 2)
+	}
 }
